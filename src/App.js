d070d064bfac4dee33e25db97884e942d1c1c2c3
Hyunho Son <aa000098@naver.com>
2024-03-19 13:59:44 +0900

    feat: 구글 애널리틱스 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
 import { ThemeProvider } from "styled-components"
+import ReactGA from "react-ga";
 
 import GlobalStyle from "./GlobalStyle"
 import theme from "./theme"
@@ -8,6 +9,10 @@ import theme from "./theme"
 import ScrollToTop from "./Components/ScrollToTop"
 import Contact from "./Pages/Contact"
 import Root from "./Pages/Root"
+
+const gaTrackingId = process.env.REACT_APP_GA_TRACKING_ID;
+ReactGA.initialize(gaTrackingId, {debug: true});
+ReactGA.pageview(window.location.pahtname);
 
 const App = () => {
     return (
